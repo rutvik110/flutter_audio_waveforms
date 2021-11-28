0749d5516012209818142272d8710456b9675a72
rutvik110 <takrutvik@gmail.com>
2021-11-28 13:52:43 +0530

    updated painting algorithm fixed the shader bug by updating alignment

--- lib/painters/rounded_waveform_painter.dart
+++ lib/painters/rounded_waveform_painter.dart
@@ -21,6 +21,13 @@ class RoundedWaveformPainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
+    List<double> processedSamples =
+        samples.map((e) => e.abs() * size.height).toList();
+    final maxNum = processedSamples.reduce(math.max);
+    final double multiplier = math.pow(maxNum, -1).toDouble();
+
+    List<double> processedSamples2 =
+        processedSamples.map((e) => e * multiplier * size.height / 2).toList();
     final double pointWidth = size.width / samples.length;
     //fix the strokewidth issue
     final paint = Paint()
@@ -30,8 +37,8 @@ class RoundedWaveformPainter extends CustomPainter {
       ..strokeCap = StrokeCap.round
       ..strokeJoin = StrokeJoin.round
       ..shader = LinearGradient(
-          begin: Alignment.centerLeft,
-          end: Alignment.centerRight,
+          begin: Alignment(-1.001, 0.0),
+          end: Alignment(1.001, 0.0),
           // transform: GradientRotation(math.pi / 2),
           colors: [
             Color(0xFFff3400),
@@ -54,29 +61,29 @@ class RoundedWaveformPainter extends CustomPainter {
     //   ..shader = shader;
 
     final waveFormPath = Path();
-    List<double> processedSamples =
-        samples.map((e) => e.abs() * size.height).toList();
-    final maxNum = processedSamples.reduce(math.max);
-    final double multiplier = math.pow(maxNum, -1).toDouble();
 
-    List<double> processedSamples2 =
-        processedSamples.map((e) => e * multiplier).toList();
     waveFormPath.moveTo(0, size.height / 2);
     for (var i = 0; i < processedSamples2.length; i++) {
       final value = processedSamples2[i];
       final double x = pointWidth * i;
       final double y = size.height / 2;
       final double x2 = pointWidth * (i + 1);
-      final double y2 = i.isEven
-          ? -value * size.height / 2 + size.height / 2
-          : value * size.height / 2 + size.height / 2;
-
+      final double y2 =
+          i.isEven ? -value + size.height / 2 : value + size.height / 2;
       final double diameter = x2 - x;
       final double radius = diameter / 2;
-      waveFormPath.lineTo(x, y);
       waveFormPath.lineTo(x, y2);
-      waveFormPath.quadraticBezierTo(
-          x2 - radius, i.isEven ? y2 - diameter : y2 + diameter, x2, y2);
+      waveFormPath.lineTo(x, i.isEven ? y2 - diameter : y2 + diameter);
+      waveFormPath.addArc(
+        Rect.fromCircle(
+          center: Offset(x2 - radius, i.isEven ? y2 - diameter : y2 + diameter),
+          radius: radius,
+        ),
+        -math.pi,
+        i.isEven ? math.pi : -math.pi,
+      );
+      // quadraticBezierTo(
+      //     x2 - radius, i.isEven ? y2 - diameter : y2 + diameter, x2, y2);
       waveFormPath.lineTo(x2, y2);
     }
     canvas.drawPath(waveFormPath, paint);
