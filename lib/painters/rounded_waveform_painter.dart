2f751fde33fc7e27c570ffec2f570a001289e7dd
rutvik110 <takrutvik@gmail.com>
2021-11-28 13:58:57 +0530

    removed dead code

--- lib/painters/rounded_waveform_painter.dart
+++ lib/painters/rounded_waveform_painter.dart
@@ -3,8 +3,8 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
 
-class RoundedWaveformPainter extends CustomPainter {
-  RoundedWaveformPainter({
+class SquigglyWaveformPainter extends CustomPainter {
+  SquigglyWaveformPainter({
     required this.samples,
     required this.xAudioPosition,
     required this.sliderValue,
@@ -29,17 +29,15 @@ class RoundedWaveformPainter extends CustomPainter {
     List<double> processedSamples2 =
         processedSamples.map((e) => e * multiplier * size.height / 2).toList();
     final double pointWidth = size.width / samples.length;
-    //fix the strokewidth issue
+
     final paint = Paint()
       ..style = PaintingStyle.stroke
       ..strokeWidth = strokeWidth
-      // ..color = Colors.white
       ..strokeCap = StrokeCap.round
       ..strokeJoin = StrokeJoin.round
       ..shader = LinearGradient(
           begin: Alignment(-1.001, 0.0),
           end: Alignment(1.001, 0.0),
-          // transform: GradientRotation(math.pi / 2),
           colors: [
             Color(0xFFff3400),
             Colors.blue
@@ -51,22 +49,12 @@ class RoundedWaveformPainter extends CustomPainter {
         Rect.fromLTWH(0, 0, size.width, size.height),
       );
 
-    //  painter for continuous path
-    // final continousActivePaint = Paint()
-    //   ..style = PaintingStyle.stroke
-    //   ..strokeWidth = strokeWidth
-    //   ..color = Colors.red
-    //   ..strokeCap = StrokeCap.round
-    //   ..strokeJoin = StrokeJoin.round
-    //   ..shader = shader;
-
     final waveFormPath = Path();
 
     waveFormPath.moveTo(0, size.height / 2);
     for (var i = 0; i < processedSamples2.length; i++) {
       final value = processedSamples2[i];
       final double x = pointWidth * i;
-      final double y = size.height / 2;
       final double x2 = pointWidth * (i + 1);
       final double y2 =
           i.isEven ? -value + size.height / 2 : value + size.height / 2;
@@ -82,37 +70,13 @@ class RoundedWaveformPainter extends CustomPainter {
         -math.pi,
         i.isEven ? math.pi : -math.pi,
       );
-      // quadraticBezierTo(
-      //     x2 - radius, i.isEven ? y2 - diameter : y2 + diameter, x2, y2);
       waveFormPath.lineTo(x2, y2);
     }
     canvas.drawPath(waveFormPath, paint);
-
-    //either move active track to its separate painter or make it a separate pain function
-    //active track
-    // List<double> movingPointsList =
-    //     List.generate(xAudio, (index) => samples[index]);
-    // final activePath = Path();
-
-    // for (var i = 0; i < movingPointsList.length; i++) {
-    //   final value = movingPointsList[i].abs();
-    //   final double x = width * i;
-    //   const double y = 0;
-    //   final double x2 = width * (i + 1);
-    //   final double y2 = i.isEven ? -value * size.height : value * size.height;
-    //   final double diameter = x2 - x;
-    //   final double radius = diameter / 2;
-    //   activePath.lineTo(x, y);
-    //   activePath.lineTo(x, y2);
-    //   activePath.quadraticBezierTo(
-    //       x2 - radius, i.isEven ? y2 - diameter : y2 + diameter, x2, y2);
-    //   activePath.lineTo(x2, y2);
-    // }
-    // canvas.drawPath(activePath, continousActivePaint);
   }
 
   @override
-  bool shouldRepaint(covariant RoundedWaveformPainter oldDelegate) {
+  bool shouldRepaint(covariant SquigglyWaveformPainter oldDelegate) {
     // TODO: implement shouldRepaint
     return true;
   }
