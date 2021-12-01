ad9a4776ebfe9d65bf9c6aa996f8420c6fdaa6c4
rutvik110 <takrutvik@gmail.com>
2021-12-01 16:26:50 +0530

    updated painting algorithm and moved canvas to center of the parent

--- lib/painters/rectangle_waveform_painter.dart
+++ lib/painters/rectangle_waveform_painter.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 class RectangleWaveformPainter extends CustomPainter {
   RectangleWaveformPainter({
@@ -12,6 +13,15 @@ class RectangleWaveformPainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
+    canvas.translate(0, size.height / 2);
+    List<double> processedSamples =
+        samples.map((e) => e * size.height).toList();
+    final maxNum = processedSamples.reduce(math.max);
+    final double multiplier = math.pow(maxNum, -1).toDouble();
+
+    List<double> processedSamples2 =
+        processedSamples.map((e) => e * multiplier * size.height / 2).toList();
+
     final paint = Paint()
       ..style = PaintingStyle.fill
       ..strokeWidth = 1.0
@@ -19,9 +29,8 @@ class RectangleWaveformPainter extends CustomPainter {
           begin: Alignment.topCenter,
           end: Alignment.bottomCenter,
           colors: [
-            Color(0xFFff3400),
-            Color(0xFFff6d00),
-            // Color(0xFFFFB5A0),
+            Colors.white,
+            Colors.white
           ],
           stops: [
             0,
@@ -34,21 +43,49 @@ class RectangleWaveformPainter extends CustomPainter {
       ..color = Color(0xFFb1cad5)
       ..strokeWidth = 0.2;
 
-    List<Offset> offsets = [];
-    final double width = size.width / samples.length;
+    final double rectangelWidth = size.width / processedSamples2.length;
 
-    for (var i = 0; i < samples.length; i++) {
-      final double x = i == samples.length - 1 ? size.width : width * i;
-      final double y = samples[i] * size.height;
-      final double x2 =
-          i + 1 == samples.length ? width * (i * 2) : width * (i + 1);
-      final double y2 = i + 1 == samples.length
-          ? samples[i] * size.height
-          : samples[i + 1] * size.height;
+    for (var i = 0; i < processedSamples2.length; i++) {
+      final double x = rectangelWidth * i;
+      final double y = processedSamples2[i];
 
-      canvas.drawRect(Rect.fromLTRB(x, 0, x2, y2 == 0 ? 1 : y2), paint);
-      canvas.drawRect(Rect.fromLTRB(x, 0, x2, y2 == 0 ? 1 : y2), strokePaint);
+      canvas.drawRect(
+        Rect.fromLTWH(x, 0, rectangelWidth, y == 0 ? 1 : y),
+        paint,
+      );
+      canvas.drawRect(
+        Rect.fromLTWH(x, 0, rectangelWidth, y == 0 ? 1 : y),
+        strokePaint,
+      );
     }
+    // final activeTrackPaint = Paint()
+    //   ..style = PaintingStyle.fill
+    //   ..strokeWidth = 1
+    //   ..shader = const LinearGradient(
+    //       begin: Alignment.topCenter,
+    //       end: Alignment.bottomCenter,
+    //       colors: [
+    //         Color(0xFFff3400),
+    //         Color(0xFFff6d00),
+    //       ],
+    //       stops: [
+    //         0,
+    //         1,
+    //       ]).createShader(
+    //     Rect.fromLTWH(0, 0, size.width, size.height),
+    //   );
+
+    // List<double> movingPointsList =
+    //     List.generate(sliderValue, (index) => processedSamples2[index]);
+
+    // for (var i = 0; i < movingPointsList.length; i++) {
+    //   final double x = rectangelWidth * i;
+    //   //make y abs to have one sided waveform
+    //   final double y = processedSamples2[i];
+
+    //   canvas.drawRect(Rect.fromLTWH(x, 0, rectangelWidth, y == 0 ? 1 : y),
+    //       activeTrackPaint);
+    // }
   }
 
   @override
