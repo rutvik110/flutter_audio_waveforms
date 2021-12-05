f224a391518c325f553ae091916eed5c0d59c3d1
rutvik110 <takrutvik@gmail.com>
2021-12-05 20:40:02 +0530

    refactoring updates

--- lib/flutter_audio_waveforms.dart
+++ lib/flutter_audio_waveforms.dart
@@ -1,106 +1,161 @@
-// library flutter_audio_waveforms;
+library flutter_audio_waveforms;
 
-// import 'dart:developer';
-// import 'dart:ui';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_audio_waveforms/waveforms/polygon_waveform/polygon_waveform.dart';
-// import 'package:flutter_audio_waveforms/waveforms/rectangle_waveform/rectangle_waveform.dart';
-// import 'package:flutter_audio_waveforms/waveforms/squiggly_waveform/squiggly_waveform.dart';
+import 'dart:developer';
+import 'dart:math' as math;
+import 'package:flutter/material.dart';
+import 'package:flutter_audio_waveforms/helpers/waveform_align.dart';
+import 'package:collection/collection.dart';
 
-// class AudioWaveforms extends StatefulWidget {
-//   const AudioWaveforms({
-//     Key? key,
-//     required this.maxDuration,
-//     required this.elapsedDuration,
-//     required this.samples,
-//     required this.height,
-//     required this.width,
-//     this.shader,
-//     this.color,
-//     this.waveFormType = WaveFormType.polygon,
-//   }) : super(key: key);
+bool Function(List<double> list1, List<double> list2) checkforSamplesEquality =
+    const ListEquality<double>().equals;
 
-//   final Duration maxDuration;
-//   final Duration elapsedDuration;
-//   final List<double> samples;
-//   final double width;
-//   final double height;
-//   final Shader? shader;
-//   final Color? color;
-//   final WaveFormType waveFormType;
+abstract class AudioWaveform extends StatefulWidget {
+  const AudioWaveform({
+    Key? key,
+    required this.samples,
+    required this.height,
+    required this.width,
+    required this.maxDuration,
+    required this.elapsedDuration,
+    required this.showActiveWaveform,
+    this.absolute = false,
+    this.invert = false,
+  })  : waveformAlign = absolute
+            ? invert
+                ? WaveformAlign.top
+                : WaveformAlign.bottom
+            : WaveformAlign.center,
+        super(key: key);
 
-//   @override
-//   State<AudioWaveforms> createState() => _AudioWaveformsState();
-// }
+  final List<double> samples;
+  final double height;
+  final double width;
+  final Duration maxDuration;
+  final Duration elapsedDuration;
+  final bool absolute;
+  final bool invert;
+  final bool showActiveWaveform;
+  final WaveformAlign waveformAlign;
 
-// class _AudioWaveformsState extends State<AudioWaveforms> {
-//   late List<double> data;
-//   late Duration maxDuration;
-//   late Duration elapsedDuration;
-//   late int xAudio;
-//   late double sliderValue;
+  @override
+  AudioWaveformState<AudioWaveform> createState();
+}
 
-//   @override
-//   void initState() {
-//     // TODO: implement initState
-//     super.initState();
+abstract class AudioWaveformState<T extends AudioWaveform> extends State<T> {
+  late List<double> _processedSamples;
 
-//     data = widget.samples;
-//     maxDuration = widget.maxDuration;
-//     elapsedDuration = widget.elapsedDuration;
-//     xAudio = 0;
-//     sliderValue = 0;
-//   }
+  List<double> get processedSamples => _processedSamples;
 
-//   @override
-//   void didUpdateWidget(covariant AudioWaveforms oldWidget) {
-//     // TODO: implement didUpdateWidget
-//     setState(() {
-//       data = widget.samples;
-//       maxDuration = widget.maxDuration;
-//       elapsedDuration = widget.elapsedDuration;
-//       sliderValue = elapsedDuration.inMilliseconds / maxDuration.inMilliseconds;
+  late double _sampleWidth;
 
-//       xAudio = (data.length * sliderValue).toInt();
-//     });
+  double get sampleWidth => _sampleWidth;
 
-//     super.didUpdateWidget(oldWidget);
-//   }
+  @protected
+  void updateProcessedSamples(List<double> updatedSamples) {
+    _processedSamples = updatedSamples;
+  }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     if (data.isEmpty) return const SizedBox.shrink();
-//     switch (widget.waveFormType) {
-//       case WaveFormType.polygon:
-//         return PolygonWaveform(
-//           width: widget.width,
-//           height: widget.height,
-//           maxDuration: widget.maxDuration,
-//           elapsedDuration: widget.elapsedDuration,
-//           samples: data,
-//         );
-//       case WaveFormType.squiggly:
-//         return SquigglyWaveform(
-//           maxDuration: widget.maxDuration,
-//           elapsedDuration: elapsedDuration,
-//           samples: data,
-//           width: widget.width,
-//           height: widget.height,
-//         );
-//       case WaveFormType.rectangle:
-//         return RectangleWaveform(
-//           width: widget.width,
-//           height: widget.height,
-//           maxDuration: widget.maxDuration,
-//           elapsedDuration: widget.elapsedDuration,
-//           samples: data,
-//         );
-//     }
-//   }
-// }
+  int get activeIndex => _activeIndex;
 
-// enum WaveFormType {
-//   polygon,
-//   squiggly,
-//   rectangle,
-// }
+  late int _activeIndex;
+
+  List<double> get activeSamples => _activeSamples;
+
+  late List<double> _activeSamples;
+
+  Duration get maxDuration => widget.maxDuration;
+  Duration get elapsedDuration => widget.elapsedDuration;
+  bool get showActiveWaveform => widget.showActiveWaveform;
+  bool get invert => widget.absolute ? !widget.invert : widget.invert;
+  bool get absolute => widget.absolute;
+  WaveformAlign get waveformAlign => widget.waveformAlign;
+
+  @protected
+  void processSamples() {
+    List<double> rawSamples = widget.samples;
+    _processedSamples = rawSamples
+        .map((e) => absolute ? e.abs() * widget.height : e * widget.height)
+        .toList();
+
+    final maxNum =
+        _processedSamples.reduce((a, b) => math.max(a.abs(), b.abs()));
+    final double multiplier = math.pow(maxNum, -1).toDouble();
+    final finaHeight = absolute ? widget.height : widget.height / 2;
+    _processedSamples = _processedSamples
+        .map(
+          (e) => invert
+              ? -e * multiplier * finaHeight
+              : e * multiplier * finaHeight,
+        )
+        .toList();
+  }
+
+  void _calculateSampleWidth() {
+    _sampleWidth = widget.width / (_processedSamples.length);
+  }
+
+  @protected
+  void _updateXAudio({int? activeIndex}) {
+    if (activeIndex != null) {
+      _activeIndex = activeIndex;
+
+      return;
+    }
+    double elapsedTimeRatio =
+        elapsedDuration.inMilliseconds / maxDuration.inMilliseconds;
+
+    _activeIndex = (widget.samples.length * elapsedTimeRatio).round();
+  }
+
+  @protected
+  void _updateActiveSamples() {
+    _activeSamples = _processedSamples.sublist(0, _activeIndex);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _processedSamples = widget.samples;
+    _activeIndex = 0;
+    _activeSamples = [];
+    _sampleWidth = 0;
+
+    if (_processedSamples.isNotEmpty) {
+      processSamples();
+      _calculateSampleWidth();
+    }
+  }
+
+  @override
+  void didUpdateWidget(covariant T oldWidget) {
+    // TODO: implement didUpdateWidget
+    super.didUpdateWidget(oldWidget);
+    if (!checkforSamplesEquality(widget.samples, oldWidget.samples) &&
+        widget.samples.isNotEmpty) {
+      processSamples();
+      _calculateSampleWidth();
+      _updateXAudio(activeIndex: 0);
+      _updateActiveSamples();
+    }
+    if (widget.showActiveWaveform) {
+      if (widget.elapsedDuration != oldWidget.elapsedDuration) {
+        _updateXAudio();
+        _updateActiveSamples();
+      }
+    }
+    if (widget.height != oldWidget.height || widget.width != oldWidget.width) {
+      processSamples();
+      _calculateSampleWidth();
+      _updateActiveSamples();
+    }
+    if (widget.absolute != oldWidget.absolute) {
+      processSamples();
+      _updateActiveSamples();
+    }
+    if (widget.invert != oldWidget.invert) {
+      processSamples();
+      _updateActiveSamples();
+    }
+  }
+}
